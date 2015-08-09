8c62f5bbc4052ab70f51ad8e314b90396c0e502a
datLucius <jlnelsonart@gmail.com>
2015-08-09 18:39:16 -0400

    bio text, responsive

--- controller.js
+++ controller.js
@@ -4,12 +4,20 @@
   .module('portfolio')
   .controller('MainController', function ($scope, $document){
     $(document).scroll(function() {
-            if ($(this).scrollTop() <= 400) {
-                $("#floating-nav-content").slideUp(400);
-            } else {
-                $("#floating-nav-content").slideDown(600);
-            }
+             if ($(this).scrollTop() <= 400) {
+                 $("#floating-nav-content").slideUp(400);
+             } else {
+                 $("#floating-nav-content").slideDown(600);
+             }
 
-        });
+         });
+        $scope.showDetail = function () {
+          $(".detailTxt").toggleClass('hideThis');
+          $(".hiphop").toggleClass('hideThis');
+        }
+        $scope.hideDetail = function () {
+          $(".detailTxt").toggleClass('hideThis');
+          $(".hiphop").toggleClass('hideThis');
+        }
   });
 }());
